ef04723dd97ebb4a114ba8e22e3fdcfd3ebb333f
Marko Dujović <dujovicm@outlook.com>
2023-10-18 17:50:30 +0200

    Rework into a single page portfolio, matrix style, implement scrollSpy

---
+++ js/scrollspy.js
@@ -0,0 +1,23 @@
+const spyScroll = () => {
+    const sections = document.querySelectorAll('.section');
+
+    window.onscroll = () => {
+        const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+        for (let s in sections) {
+            if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPosition) {
+                const id = sections[s].id;
+                var selected = document.querySelector('.selected');
+                if (selected !== null) {
+                    selected.classList.remove('selected');
+                }
+                var next = document.querySelector(`a[href*="${id}"]`);
+                if (next !== null) {
+                    next.parentNode.classList.add('selected');
+                }
+            }
+        }
+    }
+}
+
+spyScroll();
